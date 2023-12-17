86b75954f6ae8f1d4c6c371172aa93fff5d3183a
Fersy van der Zee <fersyvanderzee@gmail.com>
2023-12-17 17:25:53 +0100

    Updated and cleaned code of functions.py

--- functions.py
+++ functions.py
@@ -1,21 +1,11 @@
 import wave
 import array
+import random
 from PIL import Image
 
 
-def find_note(value, allowed_notes: list):
-    """
-    Finds the nearest allowed note.
-
-        Args:
-            value (int, float): The value to be converted.
-            allowed_notes (list): The allowed notes in a list.
-
-        Returns:
-            int: The closest allowed pitch
-    """
-
-    pitches = {
+class MusicUtils:
+    PITCHES = {
         'A': [33, 45, 57, 69, 81, 93],
         'A#': [34, 46, 58, 70, 82, 94],
         'Bb': [34, 46, 58, 70, 82, 94],
@@ -35,162 +25,195 @@ def find_note(value, allowed_notes: list):
         'Ab': [44, 56, 68, 80, 92, 104]
     }
 
-    # Initialize variables to track the closest pitch and its difference
-    closest_pitch = None
-    min_difference = float('inf')
 
-    # Iterate through the allowed notes and find the closest pitch to the given value
-    for note in allowed_notes:
-        for pitch in pitches[note]:
-            difference = abs(value - pitch)
-            if difference < min_difference:
-                min_difference = difference
-                closest_pitch = pitch
+    @classmethod
+    def get_random_note(cls, allowed_notes: list, octave: int, length: int):
+        """
+        Picks a random note from the provided scale (allowed notes).
 
-    return closest_pitch
+            Args:
+                allowed_notes (list): The allowed notes in a list.
+                octave (int): Which octave range should be picked. Between 0 and 5 (both ends included)
+                length (int): Length of the melody.
 
+            Returns:
+                int: Pitch of the selected random note.
+        """
 
-def get_scale(note: str, mode: str):
-    """
-    Returns all allowed notes within a scale.
+        if not 0 <= octave <= 5:
+            raise ValueError('Octave not within range. Pick a number between 0 and 5 (both ends included)')
 
-        Args:
-            note (str): The root note of the scale.
-            mode (str): The mode that provides the allowed notes.
-
-        Returns:
-            list: Allowed notes based on the provided note and mode.
-    """
-
-    # Define the note order based on the note_dict
-    note_order = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-
-    mode_dict = {
-        'Major': [0, 2, 2, 1, 2, 2, 2, 1],
-        'Natural minor': [0, 2, 1, 2, 2, 1, 2, 2],
-        'Harmonic minor': [0, 2, 1, 2, 2, 1, 3, 1],
-        'Melodic minor': [0, 2, 1, 2, 2, 2, 2, 1],
-        'Dorian': [0, 2, 1, 2, 2, 2, 1, 2],
-        'Phrygian': [0, 1, 2, 2, 2, 1, 2, 2],
-        'Lydian': [0, 2, 2, 2, 1, 2, 2, 1],
-        'Mixolydian': [0, 2, 2, 1, 2, 2, 1, 2],
-        'Locrian': [0, 1, 2, 2, 1, 2, 2, 2],
-        'Ahava raba': [0, 1, 3, 1, 2, 1, 2, 2],
-        'Minor pentatonic': [0, 3, 2, 2, 3, 2],
-        'Pentatonic': [0, 2, 2, 3, 2, 3],
-        'Blues': [0, 3, 2, 1, 1, 3]
-    }
-
-    # Find the index of the given note in the note_order
-    note_index = note_order.index(note)
-
-    # Get the intervals for the selected mode
-    intervals = mode_dict[mode]
-
-    # Initialize the scale with the starting note
-    scale = []
-
-    # Build the scale by applying the intervals
-    for interval in intervals[:-1]:  # Exclude the last interval
-        note_index = (note_index + interval) % 12
-        scale.append(note_order[note_index])
-
-    return scale
+        result = [random.choice(allowed_notes) for _ in range(length)]
+        return [cls.PITCHES[note][octave] for note in result]
 
 
-def linear_map(value, from_low, from_high, to_low, to_high):
-    """
-    Linearly maps a value from one range to another.
+    @classmethod
+    def find_note(cls, value, allowed_notes: list):
+        """
+        Finds the nearest allowed note.
 
-        Args:
-            value (float): The value to be mapped.
-            from_low (float): The lower bound of the original range.
-            from_high (float): The upper bound of the original range.
-            to_low (float): The lower bound of the target range.
-            to_high (float): The upper bound of the target range.
+            Args:
+                value (int, float): The value to be converted.
+                allowed_notes (list): The allowed notes in a list.
 
-        Returns:
-            float: The mapped value in the target range.
-    """
+            Returns:
+                int: The closest allowed pitch
+        """
 
-    return (value - from_low) * (to_high - to_low) / (from_high - from_low) + to_low
+        # Initialize variables to track the closest pitch and its difference
+        closest_pitch = None
+        min_difference = float('inf')
 
+        # Iterate through the allowed notes and find the closest pitch to the given value
+        for note in allowed_notes:
+            for pitch in cls.PITCHES[note]:
+                difference = abs(value - pitch)
+                if difference < min_difference:
+                    min_difference = difference
+                    closest_pitch = pitch
 
-def dataset_to_wav(dataset, output_filename, sample_width=2, sample_rate=44100):
-    """
-    Convert a dataset of audio samples into a .wav file.
-
-        Args:
-            dataset: A list or array of audio samples (e.g., integers or floats).
-            output_filename: The name of the .wav file you want to create.
-            sample_width: The sample width in bytes (e.g., 1 for 8-bit, 2 for 16-bit). Default is 2.
-            sample_rate: The sample rate in Hz (e.g., 44100 for CD-quality audio). Default = 44100.
-
-        Returns:
-            None
-    """
-
-    # Open a new .wav file for writing
-    with wave.open(output_filename, 'w') as wav_file:
-        # Set the parameters for the .wav file
-        wav_file.setnchannels(1)  # Mono audio
-        wav_file.setsampwidth(sample_width)
-        wav_file.setframerate(sample_rate)
-        wav_file.setnframes(len(dataset))
-
-        # Write the audio data to the file
-        audio_data = array.array('h' if sample_width == 2 else 'b', dataset)
-        wav_file.writeframes(audio_data.tobytes())
+        return closest_pitch
 
 
-def get_pixel_positions(image_path: str):
-    """
-    Method for iterating through every pixel in an image. The iteration is vertical.
-    The loop breaks on the first value encountered in a vertical row, because waveforms can only have one y value
-    per x.
+class ScaleUtils:
+    @staticmethod
+    def get_scale(note: str, mode: str):
+        """
+        Returns all allowed notes within a scale.
 
-        Args:
-            image_path (str): the path and name of the file to be checked.
+            Args:
+                note (str): The root note of the scale.
+                mode (str): The mode that provides the allowed notes.
 
-        Returns:
-            list: all values that can be used in a waveform.
-    """
+            Returns:
+                list: Allowed notes based on the provided note and mode.
+        """
 
-    img = Image.open(image_path)
+        # Define the note order based on the note_dict
+        note_order = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
-    result = []
+        mode_dict = {
+            'Major': [0, 2, 2, 1, 2, 2, 2, 1],
+            'Natural minor': [0, 2, 1, 2, 2, 1, 2, 2],
+            'Harmonic minor': [0, 2, 1, 2, 2, 1, 3, 1],
+            'Melodic minor': [0, 2, 1, 2, 2, 2, 2, 1],
+            'Dorian': [0, 2, 1, 2, 2, 2, 1, 2],
+            'Phrygian': [0, 1, 2, 2, 2, 1, 2, 2],
+            'Lydian': [0, 2, 2, 2, 1, 2, 2, 1],
+            'Mixolydian': [0, 2, 2, 1, 2, 2, 1, 2],
+            'Locrian': [0, 1, 2, 2, 1, 2, 2, 2],
+            'Ahava raba': [0, 1, 3, 1, 2, 1, 2, 2],
+            'Minor pentatonic': [0, 3, 2, 2, 3, 2],
+            'Pentatonic': [0, 2, 2, 3, 2, 3],
+            'Blues': [0, 3, 2, 1, 1, 3]
+        }
 
-    # Get the dimensions of the image and store for iteration
-    width, height = img.size
+        # Find the index of the given note in the note_order
+        note_index = note_order.index(note)
 
-    # Iterate through each pixel vertically
-    for x in range(width):
-        for y in range(height):
-            # Get the RGB values of the pixel at (x, y)
-            pixel = img.getpixel((x, y))
+        # Get the intervals for the selected mode
+        intervals = mode_dict[mode]
 
-            # If the pixel is not white, store the position of that pixel
-            if pixel != (255, 255, 255, 255):
-                y_val = linear_map(y, 0, height, height, 0)
-                y_val = int(y_val)  # times 4 to make it more distinctive, edit this later.
-                result.append(y_val)
-                break  # makes sure there is only one value per vertical row
+        # Initialize the scale with the starting note
+        scale = []
 
-    return result
+        # Build the scale by applying the intervals
+        for interval in intervals[:-1]:  # Exclude the last interval
+            note_index = (note_index + interval) % 12
+            scale.append(note_order[note_index])
+
+        return scale
 
 
-def get_chord_notes(chord: str):
-    """
-    Returns the chord based on the root note.
+class MappingUtils:
+    @staticmethod
+    def linear_map(value, from_low, from_high, to_low, to_high):
+        """
+        Linearly maps a value from one range to another.
 
-        Args:
-            chord (str): the root note (like 'A' or 'Bm')
+            Args:
+                value (float): The value to be mapped.
+                from_low (float): The lower bound of the original range.
+                from_high (float): The upper bound of the original range.
+                to_low (float): The lower bound of the target range.
+                to_high (float): The upper bound of the target range.
 
-        returns:
-            list: all notes within the chord.
-    """
+            Returns:
+                float: The mapped value in the target range.
+        """
 
-    chord_notes_dict = {
+        return (value - from_low) * (to_high - to_low) / (from_high - from_low) + to_low
+
+
+class AudioUtils:
+    @staticmethod
+    def dataset_to_wav(dataset, output_filename, sample_width=2, sample_rate=44100):
+        """
+        Convert a dataset of audio samples into a .wav file.
+
+            Args:
+                dataset: A list or array of audio samples (e.g., integers or floats).
+                output_filename: The name of the .wav file you want to create.
+                sample_width: The sample width in bytes (e.g., 1 for 8-bit, 2 for 16-bit). Default is 2.
+                sample_rate: The sample rate in Hz (e.g., 44100 for CD-quality audio). Default = 44100.
+
+            Returns:
+                None
+        """
+
+        # Open a new .wav file for writing
+        with wave.open(output_filename, 'w') as wav_file:
+            # Set the parameters for the .wav file
+            wav_file.setnchannels(1)  # Mono audio
+            wav_file.setsampwidth(sample_width)
+            wav_file.setframerate(sample_rate)
+            wav_file.setnframes(len(dataset))
+
+            # Write the audio data to the file
+            audio_data = array.array('h' if sample_width == 2 else 'b', dataset)
+            wav_file.writeframes(audio_data.tobytes())
+
+
+class ImageUtils:
+    @staticmethod
+    def get_pixel_positions(image_path: str):
+        """
+        Method for iterating through every pixel in an image. The iteration is vertical.
+        The loop breaks on the first value encountered in a vertical row, because waveforms can only have one y value
+        per x.
+
+            Args:
+                image_path (str): the path and name of the file to be checked.
+
+            Returns:
+                list: all values that can be used in a waveform.
+        """
+
+        img = Image.open(image_path)
+
+        result = []
+
+        # Get the dimensions of the image and store for iteration
+        width, height = img.size
+
+        # Iterate through each pixel vertically
+        for x in range(width):
+            for y in range(height):
+                # Get the RGB values of the pixel at (x, y)
+                pixel = img.getpixel((x, y))
+
+                # If the pixel is not white, store the position of that pixel
+                if pixel != (255, 255, 255, 255):
+                    y_val = MappingUtils.linear_map(y, 0, height, height, 0)
+                    y_val = int(y_val)  # times 4 to make it more distinctive, edit this later.
+                    result.append(y_val)
+                    break  # makes sure there is only one value per vertical row
+
+        return result
+
+
+class ChordUtils:
+    CHORDS_NOTES_DICT = {
         'C': ['C', 'E', 'G'],
         'C#': ['C#', 'E#', 'G#'],
         'Db': ['Db', 'F', 'Ab'],
@@ -230,71 +253,74 @@ def get_chord_notes(chord: str):
         'Cbm': ['Cb', 'Ebb', 'Gbb']
     }
 
-    return chord_notes_dict[chord]
+
+    @staticmethod
+    def get_chord_notes(chord: str):
+        """
+        Returns the chord based on the root note.
+
+            Args:
+                chord (str): the root note (like 'A' or 'Bm')
+
+            returns:
+                list: all notes within the chord.
+        """
+
+        return ChordUtils.CHORDS_NOTES_DICT[chord]
+
+    @staticmethod
+    def get_progression(progression: str, note: str, mode: str):
+        """
+            Generate a chord progression based on a given musical note and mode.
+
+            Parameters:
+            - progression (str): A string with the progression. Example: I-V-vi-IV.
+            - note (str): The root note of the scale (e.g., 'C', 'D#', 'G').
+            - mode (str): The musical mode for the scale (e.g., 'major' or 'minor').
+
+            Returns:
+            - chords_list (list): A list of chord names corresponding to the given progression.
+        """
+        progression = progression.replace('–', '-')
+
+        scale = ScaleUtils.get_scale(note, mode)
+
+        progression = progression.split('-')
+
+        chords_list = []
+
+        for p in progression:
+            if p.isupper():
+                convert_progression = {
+                    'I': 0,
+                    'II': 1,
+                    'III': 2,
+                    'IV': 3,
+                    'V': 4,
+                    'VI': 5,
+                    'VII': 6,
+                }
+                chord = scale[convert_progression[p]]
+            else:
+                convert_progression = {
+                    'i': 0,
+                    'ii': 1,
+                    'iii': 2,
+                    'iv': 3,
+                    'v': 4,
+                    'vi': 5,
+                    'vii': 6,
+                }
+
+                chord = str(scale[convert_progression[p]]) + 'm'
+
+            chords_list.append(chord)
+
+        return chords_list
 
 
-def get_progression(progression: str, note: str, mode: str):
-    """
-        Generate a chord progression based on a given musical note and mode.
-
-        Parameters:
-        - progression (str): A string with the progression. Example: I-V-vi-IV.
-        - note (str): The root note of the scale (e.g., 'C', 'D#', 'G').
-        - mode (str): The musical mode for the scale (e.g., 'major' or 'minor').
-
-        Returns:
-        - chords_list (list): A list of chord names corresponding to the given progression.
-    """
-    progression = progression.replace('–', '-')
-
-    scale = get_scale(note, mode)
-
-    progression = progression.split('-')
-
-    chords_list = []
-
-    for p in progression:
-        if p.isupper():
-            convert_progression = {
-                'I': 0,
-                'II': 1,
-                'III': 2,
-                'IV': 3,
-                'V': 4,
-                'VI': 5,
-                'VII': 6,
-            }
-            chord = scale[convert_progression[p]]
-        else:
-            convert_progression = {
-                'i': 0,
-                'ii': 1,
-                'iii': 2,
-                'iv': 3,
-                'v': 4,
-                'vi': 5,
-                'vii': 6,
-            }
-
-            chord = str(scale[convert_progression[p]]) + 'm'
-
-        chords_list.append(chord)
-
-    return chords_list
-
-
-def get_chord_note_pitch(note: str):
-    """
-    Retrieve MIDI pitch values for a given musical note.
-
-    Parameters:
-    - note (str): The musical note for which the MIDI pitch value is needed (e.g., 'C', 'D#', 'G').
-
-    Returns:
-    - pitches (list): A list of MIDI pitch values corresponding to the input note.
-    """
-
-    pitches = {
+class MidiUtils:
+    PITCHES = {
         'A': 57,
         'A#': 58,
         'Bb': 58,
@@ -314,4 +340,16 @@ def get_chord_note_pitch(note: str):
         'Ab': 68
     }
 
-    return pitches[note]
+    @staticmethod
+    def get_chord_note_pitch(note: str):
+        """
+        Retrieve MIDI pitch values for a given musical note.
+
+        Parameters:
+        - note (str): The musical note for which the MIDI pitch value is needed (e.g., 'C', 'D#', 'G').
+
+        Returns:
+        - pitches (list): A list of MIDI pitch values corresponding to the input note.
+        """
+
+        return MidiUtils.PITCHES[note]
